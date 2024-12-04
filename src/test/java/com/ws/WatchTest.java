8488b71eaaecd1e95aafb5d57197c48d7faecc32
wushuaiqi1 <wushuaiqi1@tal.com>
2024-12-04 17:27:21 +0800

    测试UUID

--- src/test/java/com/ws/WatchTest.java
+++ src/test/java/com/ws/WatchTest.java
@@ -4,7 +4,11 @@ package com.ws;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 import org.springframework.util.StopWatch;
+
+import javax.annotation.Resource;
+import java.util.UUID;
 
 @SpringBootTest
 @Slf4j
@@ -15,9 +19,17 @@ public class WatchTest {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
-        Thread.sleep(3*1000);
+        Thread.sleep(3 * 1000);
 
         stopWatch.stop();
-        log.info("程序执行时间 {}",stopWatch.getTotalTimeMillis());
+        log.info("程序执行时间 {}", stopWatch.getTotalTimeMillis());
+    }
+
+    @Test
+    public void TestUUID() {
+        String uuid = UUID.randomUUID().toString();
+        String res = uuid.replace("-", "");
+        log.info("UUID:{},length:{},replace:{},new length{}", uuid, uuid.length(), res, res.length());
+        System.out.println(UUID.randomUUID().toString().length());
     }
 }
